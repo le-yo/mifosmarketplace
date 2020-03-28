589f0c8a9031db362a6cce5f288001b7469be05f
Le-Yo <lenykoskey@gmail.com>
2020-03-28 16:34:57 +0300

    Fixing scheduling issues

---
+++ Modules/MifosReminder/Providers/ScheduleServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\MifosReminder\Providers;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\ServiceProvider;
+
+class ScheduleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+            $schedule->call('Modules\MifosReminder\Http\Controllers\MifosReminderController@send')->everyMinute()->name('payment-reminder');
+        });
+    }
+
+    public function register()
+    {
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
