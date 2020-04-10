2fd7a7cf16cbd88257c8c0008f19982f9ba1eb8d
Le-Yo <lenykoskey@gmail.com>
2020-04-10 19:55:18 +0300

    Fixing this and that

--- Modules/MifosSms/Http/Controllers/MifosSmsController.php
+++ Modules/MifosSms/Http/Controllers/MifosSmsController.php
@@ -75,9 +75,9 @@ class MifosSmsController extends Controller
         $log->message = $message;
         $log->status = 0;
         $log->save();
-        $url = "https://rslr.connectbind.com/bulksms/bulksms?username=".$config->username."&password=".Crypt::decrypt($config->key)."@5&type=0&dlr=1&destination=".$to."&source=".$config->sender_name."&message=".urlencode($message);
+        $url = "http://rslr.connectbind.com:8080/bulksms/bulksms?username=".$config->username."&password=".Crypt::decrypt($config->key)."@5&type=0&dlr=1&destination=".$to."&source=".$config->sender_name."&message=".urlencode($message);
         $ch = curl_init();
-        $data = "";
+        $data = ""; 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         //curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
