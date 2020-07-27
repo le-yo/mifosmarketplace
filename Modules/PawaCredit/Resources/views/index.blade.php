73d855a8fb5d1cecbeb9262b888db18fe55e04e9
Leonard Korir <lenyoskey@gmail.com>
2020-07-27 14:02:04 +0300

    removed PawaCredit

--- Modules/PawaCredit/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('pawacredit::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('pawacredit.name') !!}
-    </p>
-@endsection
